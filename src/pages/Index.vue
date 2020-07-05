9e7c35407f628a727c456e0bcb483e48e8b749a8
srchach <gsikmo@gmail.com>
2020-07-05 17:03:51 -0500

    FEATURE: Obtaining a file input. first steps to convert XLSX into Json

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,11 +4,44 @@
       alt="Quasar logo"
       src="~assets/quasar-logo-full.svg"
     >
+    <q-file
+      style="max-width: 300px"
+      v-model="model"
+      filled
+      rounded
+      label="Restricted to images"
+      accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel, .xlsx, .xls"
+      @rejected="onRejected"
+      @input="readExcel"
+    >
+      <template v-if="model" v-slot:append>
+        <q-icon name="cancel" @click.stop.prevent="model = null" class="cursor-pointer" />
+      </template>
+    </q-file>
+    {{ (model !== null) ? model : 'No has seleccionado na' }}
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data: function () {
+    return {
+      model: null
+    }
+  },
+  methods: {
+    onRejected (rejectedEntries) {
+      // Notify plugin needs to be installed
+      // https://quasar.dev/quasar-plugins/notify#Installation
+      this.$q.notify({
+        type: 'negative',
+        message: `${rejectedEntries.length} file(s) did not pass validation constraints`
+      })
+    },
+    readExcel (archivo) {
+      console.log(archivo, 'leído')
+    }
+  }
 }
 </script>
