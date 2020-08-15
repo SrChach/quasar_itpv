c2a603cf662aaa4f77b9a56f347d98bb50c23a85
AlphaFG00 <96fernando.guerrero@gmail.com>
2020-08-14 22:02:15 -0500

    recieved nachito's help

--- src/pages/Scripts.vue
+++ src/pages/Scripts.vue
@@ -1,11 +1,12 @@
 <template>
+<!-- Formulario de Contraseña-->
   <q-page padding>
     <div class="q-pa-md q-gutter-sm">
           <q-card class="my-card text-white" style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)">
             <q-card-section>
-                <div class="text-h6">Borrar Todos los Productos</div>
+                <div class="text-h6">TEST1</div>
               <div class="text-subtitle2">
-                <q-btn color="white" text-color="black" label="Ejecutar"></q-btn> |
+                <q-btn color="white" text-color="black" label="Ejecutar" @click="runQuery('queryID')"></q-btn> |
                 <q-btn color="secondary" text-color="black" label="Mas Informacion"></q-btn>
               </div>
             </q-card-section>
@@ -17,7 +18,7 @@
             <q-card-section>
               <div class="text-h6"> Reiniciar Base de Datos</div>
               <div class="text-subtitle2">
-                <q-btn color="white" text-color="black" label="Ejecutar"></q-btn> |
+                <q-btn color="white" text-color="black" label="Ejecutar" @click="runQuery('queryID2')"></q-btn> |
                 <q-btn color="secondary" text-color="black" label="Mas Informacion"></q-btn>
               </div>
             </q-card-section>
@@ -64,6 +65,24 @@
 
 <script>
 export default {
-  // name: 'PageName',
+  data () {
+    return {
+      password: 'el_nashito'
+    }
+  },
+  created () {
+    this.$q.electron.ipcRenderer.on('emergency_Scripts', this.respManager)
+  },
+  beforeDestroy () {
+    this.$q.electron.ipcRenderer.removeListener('emergency_Scripts', this.respManager)
+  },
+  methods: {
+    respManager (event, data) {
+      console.log(data)
+    },
+    runQuery (queryID) {
+      this.$q.electron.ipcRenderer.send('emergencyscripts_Trigger', queryID)
+    }
+  }
 }
 </script>
