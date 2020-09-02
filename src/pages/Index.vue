3f92a20bb68031f0a595b59af7ae0deb91b22a19
srchach <gsikmo@gmail.com>
2020-09-02 14:45:36 -0500

    FEATURE: Changed logos

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,13 @@
 <template>
-  <q-page class="flex flex-center justify-center">
-    <h2>Home <q-icon name="home"/> [pending]</h2>
+  <q-page class="flex flex-center">
+    <div class="q-pa-md row justify-center q-gutter-md">
+      <div class="col-8 items-center">
+        <q-img
+          src="/icons/favicon-128x128.png"
+          style="width: 170px"
+        /><br>
+        <h4 class="text-center">Bienvenido</h4>
+      </div>
+    </div>
   </q-page>
 </template>
